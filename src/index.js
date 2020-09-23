b8dceca5131383b25e5f3734a4913e4fb97897df
fbfb6123 <fbfb6123@gmail.com>
2020-09-23 14:49:21 +0900

    xIsNextで真偽地が反転するように

--- src/index.js
+++ src/index.js
@@ -16,13 +16,16 @@ class Board extends React.Component {
         super(props);
         this.state = {
             squares: Array(9).fill(null),
+            xIsNext: true,
         };
     }
 
     handleClick(i) {
         const squares = this.state.squares.slice();
-        squares[i] = 'X';
-        this.setState({squares: squares});
+        squares[i] = this.state.xIsNext?'X':'O';
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext,});
     }
     renderSquare(i) {
         //squares という配列は Board のコンストラクタで定義しているから、Board の renderSquare がそこから値を読み込む
@@ -34,7 +37,7 @@ class Board extends React.Component {
         );
     }
     render() {
-        const status = 'Next player: X';
+        const status = 'Next player:' + (this.state.xIsNext?'X' : 'O');
 
         return (
             <div>
