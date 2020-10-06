fd37e8e13cfbbe4f26e9089545722cc631294042
fbfb6123 <fbfb6123@gmail.com>
2020-10-06 14:20:09 +0900

    index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+//　rootの要素の中にApp要素を入れ込みindex.html(root)に反映される
 ReactDOM.render(<App />, document.getElementById('root'));
 
 
