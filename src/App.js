3c4e5f17db037122f5d6a40309b84f0f4a8020ec
fbfb6123 <fbfb6123@gmail.com>
2020-10-06 14:25:03 +0900

    App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
 
 export default class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: 'James'
+        }
+    }
     render() {
+        const name = this.state.name;
+        
         return (
-            <h1>Hello, world.</h1>
+            <h1>Hello, {name}.</h1>
         );
     }
 }
