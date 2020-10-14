a66df70bd812469d4fbcaabcea4cb9d66e8b0892
fbfb6123 <fbfb6123@gmail.com>
2020-10-14 11:55:37 +0900

    index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+//メッセージ非同期表示
+/*
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
@@ -5,9 +7,75 @@ import App from './App';
 //　rootの要素の中にApp要素を入れ込みindex.html(root)に反映される
 ReactDOM.render(<App />, document.getElementById('root'));
 
+*/
 
 
 // 三目並べゲーム
 
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
 
+class Square extends React.Component {
+    render() {
+        return (
+            <button className="square">
+                {/* TODO */}
+            </button>
+        );
+    }
+}
 
+class Board extends React.Component {
+    renderSquare(i) {
+        return <Square />;
+    }
+
+    render() {
+        const status = 'Next player: X';
+
+        return (
+            <div>
+                <div className="status">{status}</div>
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
+            </div>
+        );
+    }
+}
+
+class Game extends React.Component {
+    render() {
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board />
+                </div>
+                <div className="game-info">
+                    <div>{/* status */}</div>
+                    <ol>{/* TODO */}</ol>
+                </div>
+            </div>
+        );
+    }
+}
+
+// ========================================
+
+ReactDOM.render(
+    <Game />,
+    document.getElementById('root')
+);
