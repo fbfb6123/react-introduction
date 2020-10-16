289c7bcc914de589155a074bcf46bb69e6a5972b
fbfb6123 <fbfb6123@gmail.com>
2020-10-16 17:37:30 +0900

    new dataにProviderコンテキストを付与

--- src/App.js
+++ src/App.js
@@ -4,17 +4,25 @@ import logo from './logo.svg';
 import './App.css';
 
 let data = {title:'Title',
-         message:'this is sample message!.'};
+         message:'this is sample message.'};
 
 //コンテキスストの追加
 const SampleContext = React.createContext(data);
 
 class App extends Component {
+    newdata = {title:'新しいタイトル',
+        message:'これはプロバイダーで用意した新しいメッセージ'};
 
     render() {
         return (
             <div>
                 <h1>Context</h1>
+                <Title />
+                <Message />
+                <SampleContext.Provider value={this.newdata}>
+                    <Title />
+                    <Message />
+                </SampleContext.Provider>
                 <Title />
                 <Message />
             </div>
@@ -38,17 +46,15 @@ class Title extends Component {
 class Message extends Component {
     static contextType = SampleContext;
 
-render(){
-    return (
-        <div>
-            <p>{this.context.message}</p>
-        </div>
-    );
-}
+    render(){
+        return (
+            <div>
+                <p>{this.context.message}</p>
+            </div>
+        );
+    }
 }
 export default App;
-
-
 
 
 /*//メッセージ非同期表示
