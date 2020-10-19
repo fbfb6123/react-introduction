6766acc55cd7bea9061b4d3bfaa361a9aca44f93
fbfb6123 <fbfb6123@gmai.com>
2020-10-19 10:10:33 +0900

    App.js

--- src/App.js
+++ src/App.js
@@ -8,10 +8,6 @@ import AddFrom from './memo/AddForm';
 import FindFrom from './memo/FindForm';
 import DelForm from './memo/DelForm';
 
-//ステートのマッピング
-function mappingState(state) {
-    return state;
-}
 
 //Appのコンポーネント
 class App extends Component {
@@ -23,70 +19,20 @@ class App extends Component {
     render() {
         return (
             <div>
-                <h1>Redux</h1>
-                <Message />
-                <Button />
+                <h1>Memo</h1>
+                <AddForm />
+                <hr />
+                <table><tbody><tr>
+                    <td style={this.td}><FindForm /></td>
+                    <td style={this.td}><DelForm /></td>
+                </tr></tbody></table>
+                <Memo />
             </div>
         );
     }
 }
 
-//ストアのコネクト
-App = connect() (App);
-
-//メッセージの表示コンポーネント
-class Message extends Component {
-    style = {
-        fontSize:"20pt",
-        padding:"20px 5px"
-    }
-
-    render(){
-        return (
-            <p style={this.style}>
-                {this.props.message}:{this.props.counter}
-            </p>
-        );
-    }
-}
-
-//ストアのコネクト
-//mappingStateでMessegeのコンポーネントで利用するステート(これで上のpropsの中に値が組み込まれている)
-Message = connect(mappingState) (Message);
-
-//ボタン表示のコンポーネント
-class Button extends Component {
-    style = {
-        fontSize:"16pt",
-        padding:"5px 10px"
-    }
-
-    constructor(props){
-        super(props);
-        this.doAction = this.doAction.bind(this);
-    }
-
-    //ボタンクリックでディスパッチを実行
-    doAction(e) {
-        if (e.shiftKey) {
-            this.props.dispatch({ type:'DECREMENT' });
-        } else {
-            this.props.dispatch ({ type:'INCREMENT' });
-        }
-    }
-
-    render() {
-        return (
-            <button style={this.style}
-                    onClick={this.doAction}>Click</button>
-        );
-    }
-}
-
-//ストアのコネクト
-Button = connect()(Button);
-
-export default App;
+export default connect() (App);
 
 /*//メッセージ非同期表示
 import React, { Component } from 'react';
